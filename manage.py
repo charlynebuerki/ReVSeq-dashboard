abf022558a2b81e84a91e10339bdbd692f9c6671
Mara Neacsu <mara.ioana.neacsu@gmail.com>
2024-07-04 10:01:50 +0200

    add influenza A

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'revseq_dashboard.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReVSeq-dashboard.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
